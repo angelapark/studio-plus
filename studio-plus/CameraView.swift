7b3577ec5c31d3a3cad10c300abc36108047a955
lindsaypond <lindsay.pond@nbcuni.com>
2017-03-25 16:28:04 -0700

    Move all the useful camera methods to cameraView

--- studio-plus/CameraView.swift
+++ studio-plus/CameraView.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
 
 protocol QuickViewDelegate {
     func showPhoto(image: UIImage)
-}
+
 class CameraView: UIView {
     var quickViewDelegate: QuickViewDelegate?
     @IBOutlet weak var camPreview: UIView!
@@ -19,16 +21,60 @@ class CameraView: UIView {
     
     @IBOutlet weak var switchCameraButton: UIButton!
     
+    //MARK - Properties
+    let captureSession = AVCaptureSession()
+    let imageOutput = AVCaptureStillImageOutput()
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var activeInput: AVCaptureDeviceInput!
+    
+    
+    
     class func instanceFromNib() -> UIView {
         return UINib(nibName: "Camera", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! UIView
     }
 
     @IBAction func switchCameras(_ sender: AnyObject) {
-        switchCameras?()
+        // Make sure the device has more than 1 camera.
+        if AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo).count > 1 {
+            // Check which position the active camera is.
+            var newPosition: AVCaptureDevicePosition!
+            if activeInput.device.position == AVCaptureDevicePosition.back {
+                newPosition = AVCaptureDevicePosition.front
+            } else {
+                newPosition = AVCaptureDevicePosition.back
+            }
+            
+            // Get camera at new position.
+            var newCamera: AVCaptureDevice!
+            let devices = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo)
+            for device in devices! {
+                if (device as AnyObject).position == newPosition {
+                    newCamera = device as! AVCaptureDevice
+                }
+            }
+            
+            // Create new input and update capture session.
+            do {
+                let input = try AVCaptureDeviceInput(device: newCamera)
+                captureSession.beginConfiguration()
+                // Remove input for active camera.
+                captureSession.removeInput(activeInput)
+                // Add input for new camera.
+                if captureSession.canAddInput(input) == true {
+                    captureSession.addInput(input)
+                    activeInput = input
+                } else {
+                    captureSession.addInput(activeInput)
+                }
+                captureSession.commitConfiguration()
+            } catch {
+                print("Error switching cameras: \(error)")
+            }
+        }
     }
 
-    var switchCameras: (()-> Void)?
-    
+
+
     @IBAction func toggleHint(_ sender: UIButton) {
         hintButton.isSelected = !hintButton.isSelected
     }
@@ -36,6 +82,7 @@ class CameraView: UIView {
     @IBAction func displayThumbnail(_ sender: Any) {
         quickViewDelegate?.showPhoto()
     }
+
     func configure(brandAssets: CameraBrandAssets) {
         if let defaultImage =  UIImage(named: brandAssets.hintImageDefaultName) {
             hintButton.setImage(defaultImage, for: .normal)
@@ -47,4 +94,113 @@ class CameraView: UIView {
         
         
     }
+    
+    func currentVideoOrientation() -> AVCaptureVideoOrientation {
+        var orientation: AVCaptureVideoOrientation
+        
+        switch UIDevice.current.orientation {
+        case .portrait:
+            orientation = AVCaptureVideoOrientation.portrait
+        case .landscapeRight:
+            orientation = AVCaptureVideoOrientation.landscapeLeft
+        case .portraitUpsideDown:
+            orientation = AVCaptureVideoOrientation.portraitUpsideDown
+        default:
+            orientation = AVCaptureVideoOrientation.landscapeRight
+        }
+        
+        return orientation
+    }
+    
+    func setPhotoThumbnail(_ image: UIImage) {
+        DispatchQueue.main.async { () -> Void in
+            self.thumbnail.contentMode = .scaleAspectFill
+            
+            self.thumbnail.setBackgroundImage(image, for: UIControlState())
+            self.thumbnail.layer.borderColor = UIColor.white.cgColor
+            self.thumbnail.layer.borderWidth = 1.0
+            
+        }
+    }
+    
+    func savePhotoToLibrary(_ image: UIImage) {
+        let photoLibrary = PHPhotoLibrary.shared()
+        photoLibrary.performChanges({
+            PHAssetChangeRequest.creationRequestForAsset(from: image)
+        }) { (success: Bool, error: Error?) -> Void in
+            if success {
+                // Set thumbnail
+                self.setPhotoThumbnail(image)
+            } else {
+                print("Error writing to photo library: \(error!.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func capturePhoto(_ sender: AnyObject) {
+        let connection = imageOutput.connection(withMediaType: AVMediaTypeVideo)
+        if (connection?.isVideoOrientationSupported)! {
+            connection?.videoOrientation = currentVideoOrientation()
+        }
+        
+        imageOutput.captureStillImageAsynchronously(from: connection, completionHandler: { (sampleBuffer, error) in
+            if sampleBuffer != nil {
+                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                let image = UIImage(data: imageData!)
+                self.savePhotoToLibrary(image!)
+            } else {
+                print("Error capturing photo: \(error?.localizedDescription)")
+            }
+        })
+    }
+    
+    func videoQueue() -> DispatchQueue {
+        return DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.default)
+    }
+    
+    func setupPreview() {
+        // Configure previewLayer
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = camPreview.bounds
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        camPreview.layer.addSublayer(previewLayer)
+    }
+    
+    func setupSession() {
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        let camera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: camera)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+                activeInput = input
+            }
+        } catch {
+            print("Error setting device input: \(error)")
+        }
+        
+        imageOutput.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+        if captureSession.canAddOutput(imageOutput) {
+            captureSession.addOutput(imageOutput)
+        }
+        
+    }
+    
+    func startSession() {
+        if !captureSession.isRunning {
+            videoQueue().async {
+                self.captureSession.startRunning()
+            }
+        }
+    }
+    
+    func stopSession() {
+        if captureSession.isRunning {
+            videoQueue().async {
+                self.captureSession.stopRunning()
+            }
+        }
+    }
+    
 }
